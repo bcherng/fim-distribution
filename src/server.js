d4c2f0e46a9cd7f39426046ff81e695560850771
bcherng <33669812+bcherng@users.noreply.github.com>
2025-09-19 12:11:35 -0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ app.use('/downloads', express.static(path.join(__dirname, '../public/downloads')
 app.get('/', (req, res) => {
   res.send(`
     <h1>FIM Distribution</h1>
-    <p><a href="/downloads/fim-daemon.deb">Linux Installer (.deb)</a></p>
+    <p><a href="/downloads/fim-daemon.deb">Linux Installer(.deb)</a></p>
     <p><a href="/downloads/fim-daemon-setup.exe">Windows Installer(.exe)</a></p>
   `);
 });
