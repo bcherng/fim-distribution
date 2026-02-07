c906ee3e9a8c69c474ae6f5a6d6bec4ba78c96fa
bcherng <briancherngsch@gmail.com>
2026-02-07 11:33:20 -0800

    Fixed event approval route

--- src/modules/events/routes.js
+++ src/modules/events/routes.js
@@ -7,6 +7,6 @@ const router = express.Router();
 router.post('/report', requireDaemonAuth, eventsController.reportEvent);
 router.post('/acknowledge', requireDaemonAuth, eventsController.acknowledgeEvent);
 router.get('/:client_id', requireAdminAuth, eventsController.getClientEvents);
-router.post('/review/:id', requireAdminAuth, eventsController.reviewEvent);
+router.post('/:id/review', requireAdminAuth, eventsController.reviewEvent);
 
 export default router;
--- src/modules/uptime/routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import * as uptimeController from './controller.js';
-import { requireAdminAuth } from '../../middleware/auth.js';
-
-const router = express.Router();
-
-router.get('/:id', requireAdminAuth, uptimeController.getUptimeHistory);
-
-export default router;
