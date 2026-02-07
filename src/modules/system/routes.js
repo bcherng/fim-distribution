66fdf0e650c8b41fb7b02e1a3ae3c5a0a51a04ba
bcherng <briancherngsch@gmail.com>
2026-02-07 11:26:47 -0800

    Removed legacy function

--- src/modules/system/routes.js
+++ src/modules/system/routes.js
@@ -1,12 +1,10 @@
 import express from 'express';
 import * as systemController from './controller.js';
 import * as diagController from './diag_controller.js';
-import * as uptimeController from '../uptime/controller.js';
 
 const router = express.Router();
 
 router.get('/config', systemController.getConfig);
-router.get('/cron/compile-uptime', uptimeController.compileUptime);
 router.get('/diag/schema', diagController.checkSchema);
 
 export default router;
